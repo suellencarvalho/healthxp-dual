710eb4820342beaa1b61b1ab3a90b871982a271d
Suellen Carvalho <suellenncontato@gmail.com>
2023-07-24 15:58:30 -0300

    commit com steps do desafio

--- web/cypress/e2e/students.cy.js
+++ web/cypress/e2e/students.cy.js
@@ -20,7 +20,7 @@ describe('alunos', () => {
 
     })
 
-    it('não deve cadastrar com e-mail duplicado', () =>{
+    it('não deve cadastrar com e-mail duplicado', () => {
         const student = students.duplicate
 
 
@@ -33,7 +33,7 @@ describe('alunos', () => {
         studentPage.popup.haveText('O email informado já foi cadastrado!')
     })
 
-    it('deve remover um aluno sem matricula', () =>{
+    it('deve remover um aluno sem matricula', () => {
 
         const student = students.remove
 
@@ -47,19 +47,70 @@ describe('alunos', () => {
         studentPage.popup.haveText('Exclusão realizada com sucesso.')
     })
 
-    it('todos os campos são obrigatórios', () =>{
+    it('todos os campos são obrigatórios', () => {
         const student = students.required
 
         cy.adminLogin()
         studentPage.goToRegister()
         studentPage.submitForm(student)
-        studentPage.requiredMessage('Nome completo','Nome é obrigatório')
-        studentPage.requiredMessage('E-mail','O email é obrigatório')
-        studentPage.requiredMessage('Idade','A idade é obrigatória')
-        studentPage.requiredMessage('Peso (em kg)','O peso é obrigatório')
-        studentPage.requiredMessage('Altura','A altura é obrigatória')
-        
- 
+        studentPage.requiredMessage('Nome completo', 'Nome é obrigatório')
+        studentPage.requiredMessage('E-mail', 'O email é obrigatório')
+        studentPage.requiredMessage('Idade', 'A idade é obrigatória')
+        studentPage.requiredMessage('Peso (em kg)', 'O peso é obrigatório')
+        studentPage.requiredMessage('Altura', 'A altura é obrigatória')
+
     })
+
+    // Tentar cadastrar um aluno menor de 16 anos:
+
+    it('não deve cadastrar aluno menor de 16 anos', () => {
+
+        const student = students.aluno_menor
+        // Dado que estou logado como Administrador da academia
+        cy.adminLogin()
+        // E que acessei o formulário de cadastro de alunos
+        studentPage.goToRegister()
+        // Quando tento cadastrar um aluno com idade igual ou menor a 16 anos
+        studentPage.submitForm(student)
+
+        // Então o sistema não permite o cadastro, exibindo uma mensagem de alerta no campo idade
+        studentPage.requiredMessage('Idade', 'A idade mínima para treinar é 16 anos!')
+
+    })
+
+// Tentar cadastrar informando peso incorreto:
+    it('não deve cadastrar com peso incorreto', () =>{
+        const student = students.peso_zero
+        cy.task('deleteStudent', student.email)
+        // Dado que estou logado como Administrador da academia
+        cy.adminLogin()
+        // E que acessei o formulário de cadastro de alunos
+        studentPage.goToRegister()
+        // Quando tento cadastrar um aluno informando um peso menor ou igual a ZERO
+        studentPage.submitForm(student)
+
+        // Então o sistema não permite o cadastro, exibindo uma mensagem de alerta no campo idade
+        studentPage.requiredMessage('Peso (em kg)', 'O peso é obrigatório')
+    })
+
+    // Tentar cadastrar informando altura incorreta:
+    it('não deve cadastrar com altura incorreta', () =>{
+        const student = students.altura_zero
+
+        cy.task('deleteStudent', student.email)
+        // Dado que estou logado como Administrador da academia
+        cy.adminLogin()
+        // E que acessei o formulário de cadastro de alunos
+        studentPage.goToRegister()
+        // Quando tento cadastrar um aluno informando um peso menor ou igual a ZERO
+        studentPage.submitForm(student)
+
+        // Então o sistema não permite o cadastro, exibindo uma mensagem de alerta no campo idade
+        studentPage.requiredMessage('Altura', 'Altura minima permitida 1.45' )
+    })
+    // Dado que estou logado como Administrador da academia
+    // E que acessei o formulário de cadastro de alunos
+    // Quando tento cadastrar um aluno informando uma altura menor ou igual a ZERO
+    // Então o sistema não permite o cadastro, exibindo uma mensagem de alerta no campo altura
 
 })
